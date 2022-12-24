f1914137c2397df2e134f2695fce04acae0dfc1c
RamosCarlosEduardo <carlosramos.engenharia@gmail.com>
2022-12-23 22:27:05 -0300

    Merry Christmas!

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ let audio = new Audio('christmas-song.mp3');
 buttonPlay.addEventListener('click', () => {
     console.log('Play song')
     audio.play();
+
+    buttonPlay.classList.remove('active')
+    buttonPause.classList.remove('active')
+    
+    buttonPlay.classList.add('active')
 })
 
 
 buttonPause.addEventListener('click', () => {
     console.log('Pause song')
     audio.pause();
+
+    buttonPlay.classList.remove('active')
+    buttonPause.classList.remove('active')
+
+
+    buttonPause.classList.add('active')
 })
